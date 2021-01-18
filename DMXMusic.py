1cf51a451132246a324f831564dbc3c564a0d7e5
pmascal <70284360+pmascal@users.noreply.github.com>
2021-01-18 14:51:45 -0600

    Optimizations, added timing feature

--- DMXMusic.py
+++ DMXMusic.py
@@ -5,11 +5,10 @@
 import numpy as np
 from scipy import signal
 from scipy.io import wavfile
-import matplotlib.pyplot as plt
 
 class DMXMusic:
     
-    def __init__(self, wavfilename, brightness_ch = 1, red_ch=2, green_ch=3, blue_ch=4, red_max=None, green_min = None, green_max = None, blue_min = None):
+    def __init__(self, wavfilename, user_ch = False, user_rgb = False):
         """
         creates DMXMusic object
             ----------
@@ -19,28 +18,30 @@ class DMXMusic:
             brightness,red,green,blue_ch = DMX channel designated, not 0 indexed (optional)
             red,green,blue_min,max = the ranges of RGB frequencies (optional)
             red is lowest, then green, then blue.
-            
         """
-        self.brightness_ch = brightness_ch
-        self.red_ch = red_ch
-        self.green_ch = green_ch
-        self.blue_ch = blue_ch
         
-        self.red_max = red_max
-        self.green_min = green_min
-        self.green_max = green_max
-        self.blue_min = blue_min
+        if not user_ch:
+            self.brightness_ch = 1
+            self.red_ch = 2
+            self.green_ch = 3
+            self.blue_ch = 4
+        else:
+            self.brightness_ch = input("Brightness Channel: ")
+            self.red_ch = input("Red Channel: ")
+            self.green_ch = input("Green Channel: ")
+            self.blue_ch = input("Blue Channel: ")
+        
+        # self.user_rgb = user_rgb
+        if user_rgb:
+            self.red_max = input("Max Frequency for Red: ")
+            self.green_min = input("Min Freqency for Green: ")
+            self.green_max = input("Max Frequency for Green: ")
+            self.blue_min = input("Min Frequency for Blue")
         
         # Call the functions to create all the data
         self.Spectrogram(wavfilename)
         self.AverageValues()
-        self.FrameData()
-        
-        # If accurate rate to call this code from DMXBinary group is wanted.
-        self.tStart = self.times[0]
-        self.tEnd = self.times[-1]
-        self.tStep = self.times[1]-self.times[0]
-    
+        self.FrameData(user_rgb)
         
         
 #############################################################################
@@ -60,7 +61,7 @@ class DMXMusic:
 
     def AverageValues(self):
         """
-        Calculate the overall average frequency for each frame
+        Calculate the overall average frequency/amplitude for each frame
         """
         
         self.avg_vals = []
@@ -78,48 +79,40 @@ class DMXMusic:
 #############################################################################
 
 
-    def FrameData(self):
+    def FrameData(self,user_rgb):
         """
-        Create 2-D array that houses each frame's data.
+        Scale each value and assign to their frame.
         """
         
-        # Create empty 2-D array the size that binary code is expecting.
-        self.frame_data = np.zeros((len(self.times),512), dtype = int)
+        # Initialize a dictionary that will have keys be the times that define
+        # each frame, and values be brightness, red, green, and blue values.
+        self.frame_data = {}
         
         # Set the scaled range to be from 0 to 255.
         a = 0
         b = 255
         
-        # Calculate the min and max of the audio's frequencies.
+        # Calculate the min and max of the audio's frequencies and amplitudes.
         avgfreq_min, avgfreq_max = int(min(self.avg_vals)), int(max(self.avg_vals))
-        
-        # Create a scaled range of the frequencies, just for testing in the
-        # matplotlib code to compare to RGB/brightness values.
-        # self.scaled_avg = []
-        # for freq in self.avg_vals:
-        #     self.scaled_avg.append((b-a)*(freq-avgfreq_min)/(avgfreq_max - avgfreq_min)+a)
-        
-        # Create a scaled range of the amplitudes, for brightness levels.
         ampfreq_min, ampfreq_max = int(min(self.amp_vals)), int(max(self.amp_vals))
-        self.scaled_amp = []
-        for amp in self.amp_vals:
-            self.scaled_amp.append((b-a)*(amp-ampfreq_min)/(ampfreq_max - ampfreq_min)+a)
-        
-        
+       
         # A user is capable of giving the frequency bin parameters initially,
         # but if none were given, calculate based on range of frequencies.
-        if self.red_max == None:
+        if not user_rgb:
             self.red_max = int(avgfreq_max/2)
-        if self.green_min == None:
             self.green_min = int(self.red_max/2)
-        if self.blue_min == None:
             self.blue_min = int(self.red_max)
-        if self.green_max == None:
             self.green_max = int(0.5*(avgfreq_max-self.blue_min)+self.red_max)
         
         
         # Give each frame RGB and brightness values
         for i in range(len(self.avg_vals)):
+            
+            # Assign they key (time) to a list of 4 0's
+            self.frame_data[self.times[i]] = [0]*4
+            
+            # Adds the brightness level, depends on average amplitude
+            self.frame_data[self.times[i]][self.brightness_ch-1] = [(b-a)*(self.amp_vals[i]-ampfreq_min)/(ampfreq_max - ampfreq_min)+a]+[0]*3
             
             # Scale each color's value based on frequency bin parameters as 
             # well as overall min/max of audio file.
@@ -131,31 +124,29 @@ class DMXMusic:
             # If the frequency is below/above the color's min/max boundary,
             # its value will be below 0 or above 255, respectively.
             if red_val > 0 and red_val < 255:
-                self.frame_data[i][self.red_ch - 1] = red_val
+                self.frame_data[self.times[i]][self.red_ch - 1] = red_val
             if green_val > 0 and green_val < 255:
-                self.frame_data[i][self.green_ch-1] = green_val
+                self.frame_data[self.times[i]][self.green_ch-1] = green_val
             if blue_val > 0 and blue_val < 255:
-                self.frame_data[i][self.blue_ch-1] = blue_val
+                self.frame_data[self.times[i]][self.blue_ch-1] = blue_val
             
-            
-            # Adds the brightness level, depends on average amplitude
-            self.frame_data[i][self.brightness_ch - 1] = self.scaled_amp[i]
-        
-    
-    
 #############################################################################
     
-
     def tcolor(self, t):
         """
         Returns a single frame's data relevant to any time given.
         """
         
-        if t < self.times[0] or t > self.times[-1]:
-            return 0
+        if t < 0 or t > self.times[-1]:
+            raise Exception('Time given was outside audio sample')
+        elif t<self.times[0]:
+            return np.array(self.frame_data[self.times[0]]+[0]*508)
         else:
-            return self.frame_data[np.where(self.times == self.times[self.times <= t][-1])[0][0]]
+            return np.array(self.frame_data[self.times[self.times <= t][-1]]+[0]*508)
         
+#############################################################################
+#############################################################################
+#############################################################################
 
 # Asks the user what .wav file they would like to process
 def userinput():
@@ -167,22 +158,57 @@ def userinput():
 # Plot frequency, brightness, red, green, and blue all versus time for
 # visualization
 def plot():
-    t = times
-    a = DMX.scaled_avg
-    b = DMX.scaled_amp
-    c = FrameData[:,DMX.red_ch-1]
-    d = FrameData[:,DMX.green_ch-1]
-    e = FrameData[:,DMX.blue_ch-1]
-    plt.plot(t, a,'k', t, b,'y', t, c,'r', t, d,'g', t, e,'b')
+    import matplotlib.pyplot as plt
+    
+    avgfreq_min, avgfreq_max = int(min(DMX.avg_vals)), int(max(DMX.avg_vals))
+    scaled_avg = []
+    for freq in DMX.avg_vals:
+        scaled_avg.append((255-0)*(freq-avgfreq_min)/(avgfreq_max - avgfreq_min)+0)
+    
+    scaled_amp = []
+    red = []
+    green = []
+    blue = []
+    
+    for i in times:
+        scaled_amp.append(DMX.tcolor(i)[0])
+        red.append(DMX.tcolor(i)[1])
+        green.append(DMX.tcolor(i)[2])
+        blue.append(DMX.tcolor(i)[3])
+    
+    plt.plot(times, scaled_avg,'k', times, scaled_amp,'y', times, red,'r', times, green,'g', times, blue,'b')
     plt.legend(['Frequency', 'Brightness'], loc=0)
     plt.xlabel('seconds')
     plt.ylabel('0-255 scale')
     plt.show()
 
+def time(wavfilename):
+    from timeit import default_timer as timer
+    import statistics
+    
+    start = timer()
+    DMX=DMXMusic(wavfilename)
+    end = timer()
+    print("Initialization: ",end - start)
+    
+    t = []
+    for i in range(int(DMX.times[-1]-1)):
+        start = timer()
+        DMX.tcolor(i)
+        end = timer()
+        t.append(end-start)
+    print("Call for time: ", statistics.mean(t))
+
+#############################################################################
+#############################################################################
+#############################################################################
+
 if __name__ == "__main__":
     
     wavfilename = 'increasing.wav'
     # wavfilename = userinput()
+    
+    # time(wavfilename)
     
     DMX = DMXMusic(wavfilename)
     
@@ -193,11 +219,6 @@ if __name__ == "__main__":
     amplitude_values = DMX.amp_vals
     FrameData = DMX.frame_data
     
-    # Need to uncomment lines 98-100 to plot
     plot()
     
     # DMX_signal = DMX.tcolor(t)
-    
-    # DMX_signal[red_ch - 1] is color for red
-    # DMX_signal[green_ch - 1] is color for green
-    # DMX_signal[blue_ch - 1] is color for blue
